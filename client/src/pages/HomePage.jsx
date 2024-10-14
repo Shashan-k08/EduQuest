ce87c246eb30adc364f4cd55ebf5f7901fbb3426
Shashank Tiwari <shashanktiwari3509@gmail.com>
2024-10-14 13:32:03 +0530

    home page added

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Box, Heading, Button, VStack } from "@chakra-ui/react";
+
+const HomePage = ({ onSelectTest }) => {
+  return (
+    <Box
+      textAlign="center"
+      style={{ display: "flex", flexDirection: "column", marginTop: "10.5rem" }}
+      p={10}
+    >
+      <Heading mb={6} fontSize={21} opacity={0.7}>
+        Evaluate your knowledge and understanding about Front-end Stacks by
+        selecting a Test.
+      </Heading>
+      <VStack
+        marginTop="4rem"
+        gap="5rem"
+        justifyContent="center"
+        flexDirection="row"
+      >
+        <Button size="lg" onClick={() => onSelectTest("html")}>
+          HTML MCQs
+        </Button>
+        <Button size="lg" onClick={() => onSelectTest("reactjs")}>
+          ReactJS MCQs
+        </Button>
+        <Button size="lg" onClick={() => onSelectTest("css")}>
+          CSS MCQs
+        </Button>
+      </VStack>
+    </Box>
+  );
+};
+
+export default HomePage;
